ae9104f10b33c3ee787cf71761d78d49995962bc
Sam <rajsam003@gmail.com>
2019-11-26 11:31:46 +0530

    removing nodemon

--- controllers/auth.js
+++ controllers/auth.js
@@ -38,7 +38,7 @@ exports.signin = ( req, res) => {
             });
         }
         // generate a signed token with user id and secret 
-        const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET || jhfkjhdfkjhdkjfhkj)
+        const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET)
         //persist the token as 't' in cookie with expiry date
         res.cookie('t', token, {expire: new Date() + 9999});
         // return response with user and token to frontend client
